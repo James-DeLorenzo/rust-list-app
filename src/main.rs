2e67dd04da40d03cb9761dd636dd26c8019ae264
James DeLorenzo <jdelo417@gmail.com>
2025-11-15 21:14:47 -0500

    feat: pretty print

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code, unused_variables)]
 use serde::{Deserialize,Serialize};
 use std::{fs, io::{Read, Write}, path};
+
 // use serde_json::Result;
 
 const FILE: &str = "my_list.json";
@@ -16,15 +17,32 @@ struct ToDoList {
     items: Vec<ListItem>
 }
 
+impl ToDoList {
+    
+    fn pretty_print(&self) {
+        let mut out_list: Vec<String> = vec![];
+        for item in &self.items {
+            let completed_emoji = match item.completed {
+                true => "✅",
+                false => "❌"
+            };
+            out_list.push(format!("{} {}",item.title,completed_emoji));
+        }
+        println!("Tasks:");
+        println!("{}", out_list.join("\n"));
+    }
+
+}
+
 fn create_temp_list() -> ToDoList {
     let mut my_list: ToDoList = ToDoList {items: vec![].to_owned()};
     my_list.items.push(ListItem { title: "My First Item".to_string(), completed: false });
     return my_list
 }
 
-fn write_to_file(data: String, file_name: &str) {
+fn write_to_file(list_data: String, file_name: &str) {
     let mut file = fs::File::create(file_name).expect("to create a file");
-    file.write_all(data.as_bytes()).expect("write to file");
+    file.write_all(list_data.as_bytes()).expect("write to file");
 }
 
 fn main() {
@@ -37,9 +55,14 @@ fn main() {
             let d : ToDoList = serde_json::from_slice(&data).expect("your mom");
             d
         }
-        false => ToDoList { items: vec![] },
+        _ => {
+            println!("The file {} could not be found, initializing an empty list.", FILE);
+            let d = ToDoList { items: vec![] };           
+            d
+        } 
     };
-    println!("{:?}",file)
+    file.pretty_print();
+    // println!("{:?}",file)
     // let serialized = serde_json::to_string(&my_data).expect("serialize my_list");
     // write_to_file(serialized, FILE);
     // match file {
